cd0619f0e520a172884577af0140445d027ee4fc
isaacbis1 <isaacmorg93@gmail.com>
2025-02-21 14:50:49 +0100

    Update app.js

--- app.js
+++ app.js
@@ -22,10 +22,14 @@ import {
    1. CONFIGURAZIONE FIREBASE
    (Inserisci i tuoi valori di configurazione)
 ------------------------------------------------------------- */
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.17.2/firebase-app.js";
-import { getFirestore, collection, addDoc } from "https://www.gstatic.com/firebasejs/9.17.2/firebase-firestore.js";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Configurazione Firebase (verifica che i dati siano corretti!)
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyBZkCXioFje39FqmUFkM2GqEAcPvVo7csg",
   authDomain: "ritiro-olio.firebaseapp.com",
@@ -36,8 +40,9 @@ const firebaseConfig = {
   measurementId: "G-Q0WCW8T0B1"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
+const analytics = getAnalytics(app);
 
 // TEST: Aggiungere un documento nella raccolta "test"
 async function testFirestore() {
